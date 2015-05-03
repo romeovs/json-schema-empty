22e90a0f2e5a593f61424917ecf46745b6f10963
Romeo Van Snick <romeovs@gmail.com>
2015-05-03 17:16:02 +0200

    add code for simple schema's

--- src/index.js
+++ src/index.js
@@ -1,1 +1,105 @@
 
+// array <<
+var _array = function() {
+  // we don't know what we want so return empty array
+  return [];
+};
+// >>
+
+// boolean <<
+var _boolean = function() {
+  // just return a value
+  // randomly picked at implementation time :)
+  return false;
+};
+// >>
+
+// integer <<
+import _integer from './integer';
+// >>
+
+// number <<
+var _number = function(schema) {
+  // just return an integer
+  return _integer(schema);
+};
+// >>
+
+// null <<
+var _null = function() {
+  // this one was easy
+  return null;
+};
+// >>
+
+// object <<
+var _object = function() {
+  // todo
+  return {};
+};
+// >>
+
+// string <<
+var _string = function() {
+  // we do not know what we need
+  // so return empty string
+  return '';
+};
+// >>
+
+// create empty value based on schema <<
+var empty = function(schema) {
+  var {
+    type
+  , 'default': default_
+  , 'enum':    enum_
+  } = schema;
+
+  if ( default_ ) {
+    // if a default is given, return that
+    return default_;
+  } else if ( enum_ ) {
+    // if it is an enum, just use an enum value
+    // json schema enums must have at least one value
+    return enum_[0];
+  } else {
+
+    var t;
+    if ( type instanceof Array ) {
+      // select first one
+      // jsons type unions always have at least one element
+      t = type.sort()[0];
+    } else {
+      t = type;
+    }
+
+    switch ( t ) {
+      case 'array':
+        return _array(schema, schema);
+
+      case 'boolean':
+        return _boolean(schema, schema);
+
+      case 'integer':
+        return _integer(schema, schema);
+
+      case 'number':
+        return _number(schema, schema);
+
+      case 'null':
+        return _null(schema, schema);
+
+      case 'object':
+        return _object(schema, schema);
+
+      case 'string':
+        return _string(schema, schema);
+
+      default:
+        throw new Error(`cannot create value of type ${type}`);
+    }
+  }
+};
+// >>
+
+export default empty;
---
+++ src/integer.js
@@ -0,0 +1,79 @@
+
+// create smallest number that satisfies:
+// it bigger than minimum and it is mulitple of multipleOf
+var minmul = function(minimum, multipleOf, exclusive) {
+
+  // if we can return 0, we do that
+  if (( exclusive && minimum < 0 ) || ( !exclusive && minimum <= 0 )) {
+    return 0;
+  }
+
+  var min = exclusive ? minimum + 1 : minimum;
+  var rest = min % multipleOf;
+
+  if ( rest === 0 ) {
+    return min;
+  }
+
+  var sign  = multipleOf  / Math.abs(multipleOf);
+  var quot = (min - rest) / multipleOf;
+  return (quot + sign) * multipleOf;
+};
+
+// create smallest number that satisfies:
+// it bigger than minimum and it is mulitple of multipleOf
+var maxmul = function(maximum, multipleOf, exclusive) {
+  // this is symmtric to minmul
+  return -minmul(-maximum, multipleOf, exclusive);
+};
+
+var _integer = function(schema) {
+  // todo
+  var {
+    multipleOf
+  , minimum
+  , maximum
+  , exclusiveMinimum
+  , exclusiveMaximum
+  } = schema;
+
+  // check what is defined
+  var mo = multipleOf !== undefined
+    , mi = minimum    !== undefined
+    , ma = maximum    !== undefined
+    ;
+
+         if (  mo &&  mi &&  ma ) {
+    return 0;
+  } else if ( !mo &&  mi &&  ma ) {
+    // minimum and maximum
+    return minimum;
+  } else if (  mo && !mi &&  ma ) {
+    // multipleOf and maximum
+    return maxmul(maximum, multipleOf, exclusiveMinimum);
+  } else if (  mo &&  mi && !ma ) {
+    return minmul(minimum, multipleOf, exclusiveMaximum);
+  } else if (  mo && !mi && !ma ) {
+    // only multipleOf
+    return 0;
+  } else if ( !mo && !mi &&  ma ) {
+    // only maximum
+    if ( exclusiveMaximum ) {
+      return maximum > 0  ? 0 : maximum - 1;
+    } else {
+      return maximum >= 0 ? 0 : maximum;
+    }
+  } else if ( !mo &&  mi && !ma ) {
+    // only minimum
+    if ( exclusiveMinimum ) {
+      return minimum < 0 ? 0 : minimum + 1;
+    } else {
+      return minimum <= 0 ? 0 : minimum;
+    }
+  } else if ( !mo && !mi && !ma ) {
+    // totally free
+    return 0;
+  }
+};
+
+export default _integer;
